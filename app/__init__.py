1415abdd72a7cefc0604459f480c3fa92e793bbc
Anderson <60617083+AndersBau@users.noreply.github.com>
2026-04-27 15:59:44 -0400

    Feature/backend work (#3)
    * BACKEND | initial commit with Readme
    
    * BACKEND | initial commit new feature branch
    
    * Feature/backend work adding OpenAI (#2)
    
    * Feature/backend work (#1)
    
    * BACKEND | initial commit with Readme
    
    * BACKEND | initial commit new feature branch
    
    * BACKEND-WORK adding openai
    
    ---------
    
    Co-authored-by: ANDERSON BAUTISTA <andersonbautista@MacBookPro.lan>
    
    ---------
    
    Co-authored-by: ANDERSON BAUTISTA <andersonbautista@MacBookPro.lan>

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.config import Config
 from app.extensions import db
 from app.routes.health import health_bp
 from app.routes.documents import documents_bp
+from app.routes.questions import questions_bp
 
 
 def create_app():
@@ -14,5 +15,6 @@ def create_app():
 
   app.register_blueprint(health_bp)
   app.register_blueprint(documents_bp)
+  app.register_blueprint(questions_bp)
 
   return app
